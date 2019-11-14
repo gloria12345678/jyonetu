96acc19e08f88a6347814fb84643394da88581b9
成雪.Mu <glorialllmu@gmail.com>
2019-11-14 17:04:27 +0900

    s's
    s's

--- js/girl.js
+++ js/girl.js
@@ -1,6 +1,6 @@
 var myList = document.getElementById('getMyStaff');
 var apiList = document.getElementById('girlPhoto');
-var myApi ="https://test-cms-alpha.herokuapp.com/sites/1/profiles.json"
+var myApi ="https://test-cms-alpha.herokuapp.com/sites/4/profiles.json"
 document.write('<script src="../swiper.min.js"></script>');
 fetch(myApi)
 .then(function(response) {
@@ -31,7 +31,7 @@ fetch(myApi)
 
 
 var myList1 = document.getElementById('catchMynews');
-var myApi1 ="https://test-cms-alpha.herokuapp.com/sites/1/tweets.json"
+var myApi1 ="https://test-cms-alpha.herokuapp.com/sites/4/tweets.json"
 // console.log(myList1);
 fetch(myApi1)
 .then(function(response) {
